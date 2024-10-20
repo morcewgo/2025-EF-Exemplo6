127c545dcaf2b70a0aa472570b67371e8f5155ee
morcewgo <ribeiro.iara@aluno.ifsp.edu.br>
2024-10-19 22:36:43 -0300

    Biblioteca

---
+++ Migrations/20241020000611_MigracaoLivro.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF.Exemplo6.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoLivro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Estoque",
+                table: "Livro",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estoque",
+                table: "Livro");
+        }
+    }
+}
